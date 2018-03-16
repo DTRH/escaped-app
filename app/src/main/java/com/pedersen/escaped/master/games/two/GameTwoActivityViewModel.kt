12f51744a3b1f286e9ad56cfe7eac42d7cd10466
anderspedersen <anderspedersen83@gmail.com>
2018-03-16 08:20:21 +0100

    Expand Hint features into GameTwo

--- app/src/main/java/com/pedersen/escaped/master/games/two/GameTwoActivityViewModel.kt
+++ app/src/main/java/com/pedersen/escaped/master/games/two/GameTwoActivityViewModel.kt
@@ -1,8 +1,27 @@
 package com.pedersen.escaped.master.games.two
 
+import android.databinding.Bindable
+import com.pedersen.escaped.data.models.Hint
 import io.greenerpastures.mvvm.BaseViewModel
 
 class GameTwoActivityViewModel :  BaseViewModel<GameTwoActivityViewModel.Commands>() {
 
-    interface Commands
+    @get:Bindable
+    var hintList = ArrayList<Hint>()
+
+    fun launchHintControls() = commandHandler?.launchHintControls()
+
+    fun launchVideoControls() = commandHandler?.launchVideoControls()
+
+    fun launchRoomControls() = commandHandler?.launchRoomControls()
+
+    interface Commands {
+
+        fun launchHintControls()
+
+        fun launchVideoControls()
+
+        fun launchRoomControls()
+
+    }
 }
