ff5436b1dd78cebd60ef3456a3c47ac7398559b4
anderspedersen <anderspedersen83@gmail.com>
2018-09-27 08:03:00 +0200

    Play exit_dead if the timer runs out

--- app/src/main/java/com/pedersen/escaped/player/PlayerActivity.kt
+++ app/src/main/java/com/pedersen/escaped/player/PlayerActivity.kt
@@ -18,10 +18,10 @@ import com.pedersen.escaped.animations.PositionSpringAnimation
 import com.pedersen.escaped.animations.PositionSpringAnimation.PullingEventListener
 import com.pedersen.escaped.data.adapters.HintsAdapter
 import com.pedersen.escaped.databinding.ActivityPlayerBinding
-import com.pedersen.escaped.player.PlayerActivity.VideoElement.END_GOOD
-import com.pedersen.escaped.player.PlayerActivity.VideoElement.INTRO
+import com.pedersen.escaped.player.PlayerActivity.VideoElement.*
 import com.pedersen.escaped.utils.AppUtils
 import io.greenerpastures.mvvm.ViewModelActivity
+import timber.log.Timber
 
 class PlayerActivity : ViewModelActivity<PlayerActivityViewModel, ActivityPlayerBinding>(),
     PlayerActivityViewModel.Commands, VideoFragment.Commands {
@@ -118,6 +118,7 @@ class PlayerActivity : ViewModelActivity<PlayerActivityViewModel, ActivityPlayer
         path += when (videoElement) {
             INTRO -> R.raw.intro_film
             END_GOOD -> R.raw.exit_good
+            END_BAD -> R.raw.exit_dead
         }
 
         val videoFragment = VideoFragment.newInstance(Uri.parse(path))
@@ -127,14 +128,13 @@ class PlayerActivity : ViewModelActivity<PlayerActivityViewModel, ActivityPlayer
                 .replace(R.id.fragment_container, videoFragment)
                 .commit()
         } catch (e: Exception) {
-            // TODO Implement some error handling
+            Timber.d("Adding the video fragment threw and exception: $e")
         }
     }
 
     enum class VideoElement {
-        INTRO, END_GOOD
+        INTRO, END_GOOD, END_BAD
     }
-
 
     companion object {
 
